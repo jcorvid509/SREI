05d7977de43104978e64b28c47791ab652918f2d
jcorvid509 <jcorvid509@g.educaand.es>
2024-10-11 19:10:59 +0200

    241011_1910

--- Tema0/Actividades/res/0.5.1.py
+++ Tema0/Actividades/res/0.5.1.py
@@ -2,5 +2,3 @@ def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
   server_address = ('', 8000)
   httpd = server_class(server_address, handler_class)
   httpd.serve_forever()
-
-run()
